c87fba21d336b485fe8dcf15f5dbad2de311159b
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 14:14:22 +0800

    count down then up

--- upDownCounter.go
+++ upDownCounter.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
+func countDownUp(number1, number2 int) {
+	var startNum, endNum int
+	if number1 >= number2 {
+		startNum = number1
+		endNum = number2
+	} else {
+		startNum = number2
+		endNum = number1
+	}
+
+	for i := startNum; i > endNum-1; i-- {
+		fmt.Println(i)
+	}
+	for i := endNum; i < startNum+1; i++ {
+		fmt.Println(i)
+	}
+
+	return
+}
+
 func main() {
 	//Insert code here
+	var number1, number2 int
+	fmt.Println("Enter the first number:")
+	fmt.Scanln(&number1)
+	fmt.Println("Enter the second number:")
+	fmt.Scanln(&number2)
+
+	countDownUp(number1, number2)
 }
